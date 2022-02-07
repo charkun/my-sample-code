448f68241ea87232f8b5e63b04853aab75539e25
Charlito Mecarez Jr <70713746+developedbycharkun@users.noreply.github.com>
2022-02-07 23:31:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,46 @@
 const incrementButton = document.getElementsByClassName('increment');
 const decrementButton = document.getElementsByClassName('decrement');
-const elonMoneyText = document.querySelector('.elonMoney');
-const receipt = document.querySelector('.receipt');
+const yourMoneyLeftText = document.querySelector('.elonMoney');
+const footer = document.querySelector('.footer');
+
+console.log(document.getElementsByClassName('container'));
+
+grandTotalArray = [];
+yourMoney = 0;
+
+// According to https://www.investopedia.com/articles/investing/012715/5-richest-people-world.asp
+var billionairesList = [
+	{
+		rank: 1,
+		name: 'Elon Musk',
+		networth: 1252215450000,
+		// Source: https://usd.currencyrate.today/convert/amount-164000000-to-php.html,
+	},
+	{
+		rank: 2,
+		name: 'Bernard Arnault',
+		networth: 1015170550000,
+		// Source: https://usd.currencyrate.today/convert/amount-164000000-to-php.html,
+	},
+	{
+		rank: 3,
+		name: 'Jeff Bezos',
+		networth: 962037095000000,
+		// Source: https://usd.currencyrate.today/convert/amount-164000000-to-php.html,
+	},
+	{
+		rank: 4,
+		name: 'Bill Gates',
+		networth: 682237340000000,
+		// Source: https://usd.currencyrate.today/convert/amount-164000000-to-php.html,
+	},
+	{
+		rank: 5,
+		name: 'Larry Page',
+		networth: 625555990000000,
+		// Source: https://usd.currencyrate.today/convert/amount-164000000-to-php.html,
+	},
+];
 
 var formatter = new Intl.NumberFormat('en-US', {
 	style: 'currency',
@@ -12,12 +51,37 @@ var formatter = new Intl.NumberFormat('en-US', {
 	//maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 });
 
-grandTotalArray = [];
-elonMoney = 12311400000000;
+function getSelectedFunc() {
+	var getSelected = document.getElementById('billionaires').value;
+	console.log(getSelected);
+	for (let i = 0; i < billionairesList.length; i++) {
+		// console.log(billionairesList[i].name);
+		if (getSelected === billionairesList[i].name) {
+			// console.log(yourMoney);
 
-elonMoneyText.textContent = `Elon's fortune remaining: ${formatter.format(
-	elonMoney
-)}`;
+			yourMoney = billionairesList[i].networth;
+
+			document.querySelector('.billionaires-info').textContent = `${
+				billionairesList[i].name
+			} is the no. ${
+				billionairesList[i].rank
+			} richest person in the world with a networth of ${formatter.format(
+				billionairesList[i].networth
+			)}. Now go spend!`;
+
+			yourMoneyLeftText.textContent = `${
+				billionairesList[i].name
+			} fortune remaining: ${formatter.format(yourMoney)}`;
+
+			footer.children[1].textContent = `${
+				billionairesList[i].name
+			} remaining money ${formatter.format(yourMoney)}`;
+
+			incrementButtonFun();
+			decrementButtonFun();
+		}
+	}
+}
 
 function incrementButtonFun() {
 	for (let i = 0; i < incrementButton.length; i++) {
@@ -48,6 +112,8 @@ function incrementButtonFun() {
 			productPrice = productContainer.children[2];
 			productPriceTxtContent = productPrice.textContent
 				.replace('₱', '')
+				.replace(',', '')
+				.replace(',', '')
 				.replace(',', '');
 
 			productPriceNumber = parseInt(productPriceTxtContent); // This variable can now perform arithmetic operations
@@ -63,15 +129,18 @@ function incrementButtonFun() {
 			);
 
 			incButton = buttonClicked.parentElement.children[0];
-			if (grandTotal >= elonMoney) {
+			if (grandTotal > yourMoney) {
 				incButton.disabled = true;
+				alert(
+					'Spending amount has already been reached! Choose another billionaire'
+				);
 			} else {
 				incButton.disabled = false;
 			}
 
-			elonMoneyLeft = elonMoney - grandTotal;
-			elonMoneyText.textContent = `Elon's fortune remaining: ${formatter.format(
-				elonMoneyLeft
+			yourMoneyLeft = yourMoney - grandTotal;
+			yourMoneyLeftText.textContent = `Billionaires fortune remaining: ${formatter.format(
+				yourMoneyLeft
 			)}`;
 
 			// sub and grand total price code for HTML element
@@ -82,6 +151,23 @@ function incrementButtonFun() {
 			document.querySelector(
 				'.grandTotalText'
 			).textContent = `Grand total: ${formatter.format(grandTotal)}`;
+
+			//  (value/total value)×100%.
+
+			percentageSpent = parseFloat((grandTotal / yourMoney) * 100);
+
+			// For footer event
+			// The total budget your spent
+
+			footer.children[0].textContent = `The total budget you spent: ${formatter.format(
+				grandTotal
+			)}`;
+
+			footer.children[1].textContent = `Billionaires remaining money: ${formatter.format(
+				yourMoneyLeft
+			)}`;
+
+			footer.children[2].textContent = `You only spent ${percentageSpent} % of the total`;
 		});
 	}
 }
@@ -107,10 +193,10 @@ function decrementButtonFun() {
 			quantityValueSpan.textContent = `x ${quantityValue}`;
 
 			// Pricing code for HTML element
-			productPrice = productContainer.children[2];
-			productPriceTxtContent = productPrice.textContent
-				.replace('₱', '')
-				.replace(',', '');
+			// productPrice = productContainer.children[2];
+			// productPriceTxtContent = productPrice.textContent
+			// 	.replace('₱', '')
+			// 	.replace(',', '');
 
 			productPriceNumber = parseInt(productPriceTxtContent); // This variable can now perform arithmetic operations
 			subTotal = productPriceNumber * quantityValue;
@@ -128,15 +214,15 @@ function decrementButtonFun() {
 			);
 
 			incButton = buttonClicked.parentElement.children[1];
-			if (grandTotal >= elonMoney) {
+			if (grandTotal >= yourMoney) {
 				incButton.disabled = true;
 			} else {
 				incButton.disabled = false;
 			}
 
-			elonMoneyLeft = elonMoney - grandTotal;
-			elonMoneyText.textContent = `Elon's fortune remaining: ${formatter.format(
-				elonMoneyLeft
+			yourMoneyLeft = yourMoney - grandTotal;
+			yourMoneyLeftText.textContent = `Fortune remaining: ${formatter.format(
+				yourMoneyLeft
 			)}`;
 
 			// sub price code for HTML element
@@ -160,7 +246,5 @@ function decrementButtonFun() {
 	}
 }
 
-incrementButtonFun();
-decrementButtonFun();
 
 
